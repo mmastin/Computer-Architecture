ce09a59026960ad0b760af51ec2125f0fcc2817e
mmastin <matthew.mastin@gmail.com>
2020-02-28 13:52:35 -0500

    day 3/4 work

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,15 +2,19 @@
 
 import sys
 
-HLT = 0x01 # halt and exit
-LDI = 0x82 # set val to int
-PRN = 0x47 # print numeric value
-MUL = 0xA2
-ADD = 0xA0
-POP = 0x46
-PUSH = 0x45
-CALL = 0x50 # call subroutine at address stored
-RET = 0x11 # return from a subroutine
+HLT = 0b00000001 #0x01 # halt and exit
+LDI = 0b10000010 #0x82 # set val to int
+PRN = 0b01000111 #0x47 # print numeric value
+MUL = 0b10100010 #0xA2
+ADD = 0b10100000 #0xA0
+POP = 0b01000110 #0x46
+PUSH = 0b01000101 #0x45
+CALL = 0b01010000 #0x50 # call subroutine at address stored
+RET = 0b00010001 #0x11 # return from a subroutine
+CMP = 0b10100111
+JMP = 0b01010100
+JEQ = 0b01010101
+JNE = 0b01010110
 
 class CPU:
     """Main CPU class."""
@@ -20,18 +24,20 @@ class CPU:
         self.ram = [0] * 256
         self.register = [0] * 8
         self.pc = 0
-        self.register[7] = 255
+        self.running = True
+        self.register[7] = 244
         self.sp = self.register[7]
-        self.flag = 0b00000000
-        self.branctable = {
-            HLT: self.hlt,
-            LDI: self.ldi,
-            PRN: self.prn,
-            POP: self.pop,
-            PUSH: self.push,
-            CALL: self.call,
-            RET: self.ret
-        }
+        self.flag = self.register[4] #0b00000000
+        # Branchtable not currently working for some reason
+        # self.branchtable = {
+        #     HLT: self.hlt,
+        #     LDI: self.ldi,
+        #     PRN: self.prn,
+        #     POP: self.pop,
+        #     PUSH: self.push,
+        #     CALL: self.call,
+        #     RET: self.ret
+        # }
 
     def ram_read(self, address):
         return self.ram[address]
@@ -53,13 +59,41 @@ class CPU:
                     if value == '':
                         continue
 
-                    num = int(value)
-                    memory[address] = num
+                    num = int(value, 2)
+                    # self.ram[address] = num
+                    self.ram_write(num, address)
                     address += 1
 
         except FileNotFoundError:
-            print('file not found')
+            print(f"FileNotFound: {sys.argv}")
             sys.exit(2)
+
+            # with open(sys.argv[1], 'r')as f:
+            #     line = f.readlines()
+            # for line in f:
+            #     stripped = line.strip()
+        # if len(sys.argv) != 2:
+        #     print("Error")
+        #     sys.exit(1)
+        # try:
+        #     address = 0
+        #     with open(sys.argv[1]) as f:
+        #         for instruction in f:
+        #             split_excess = instruction.split('#')
+        #             split = split_excess[0].strip()
+        #             if split == '':
+        #                 continue
+        #             val = int(split, 2)
+        #             self.ram_write(address, val)
+        #             address += 1
+        # except FileNotFoundError:
+        #     print(f"FileNotFound: {sys.argv}")
+        #     sys.exit(2)
+
+
+        # except FileNotFoundError:
+        #     print('file not found')
+        #     sys.exit(2)
 
         # For now, we've just hardcoded a program:
 
@@ -82,11 +116,25 @@ class CPU:
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.register[reg_a] += self.register[reg_b]
         elif op == "MUL":
-            self.reg[reg_a] *= self.reg[reg_b]
+            self.register[reg_a] *= self.register[reg_b]
         elif op == 'SUB':
-            self.reg[reg_a] -= self.reg[reg_b]
+            self.register[reg_a] -= self.register[reg_b]
+        elif op == 'CMP':
+            # If they are equal, set the Equal E flag to 1,
+            # otherwise set it to 0.
+            # If registerA is less than registerB, set the Less-than
+            # L flag to 1, otherwise set it to 0.
+            # If registerA is greater than registerB, set the Greater-than
+            # G flag to 1, otherwise set it to 0.
+
+            if self.register[reg_a] == self.register[reg_b]:
+                self.flag = 0b00000001
+            elif self.register[reg_a] < self.register[reg_b]:
+                self.flag = 0b00000100
+            elif self.register[reg_a] > self.register[reg_b]:
+                self.flag = 0b00000010
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -106,11 +154,93 @@ class CPU:
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.register[i], end='')
 
         print()
 
     def run(self):
         """Run the CPU."""
-        while True:
-            IR = self.ram_read(self.pc)
+        while self.running is True:
+            # IR = self.ram_read(self.pc)
+            IR = self.ram[self.pc]
+            oper_a = self.ram_read(self.pc + 1)
+            oper_b = self.ram_read(self.pc + 2)
+
+            if IR == 'HLT':
+                # self.pc += 1
+                self.running = False
+                # return False
+                # self.pc += 1
+
+            if IR == 'LDI':
+                # self.branchtable[IR](oper_a, oper_b)
+                self.register[oper_a] = oper_b
+                self.pc += 3
+                # self.pc += (IR >> 6) + 1
+
+            elif IR == 'PRN':
+                print(self.register[oper_a])
+                self.pc += 2
+
+            elif IR == 'ADD':
+                self.alu('ADD', oper_a, oper_b)
+                self.pc += 3
+
+            elif IR == 'MUL':
+                self.alu('MUL', oper_a, oper_b)
+                self.pc += 3
+
+            elif IR == 'POP':
+                value = self.ram[self.sp]
+                self.register[oper_a] = value
+                self.sp += 1
+                self.pc += 2
+
+            elif IR == 'PUSH':
+                self.sp -= 1
+                value = self.register[oper_a]
+                self.ram[self.sp] = value
+                self.pc += 2
+
+            elif IR == 'SUB':
+                self.alu('SUB', oper_a, oper_b)
+                self.pc += 3
+
+            elif IR == 'CALL':
+                self.sp -= 1
+                self.ram[self.sp] = self.pc + 2
+                self.pc = self.register[oper_a]
+
+            elif IR == 'RET':
+                self.pc = self.ram[self.sp]
+                self.sp += 1
+
+            elif IR == 'JMP':
+                # Jump to the address stored in the given register.
+                # Set the PC to the address stored in the given register.
+                self.pc = self.register[oper_a]
+
+            elif IR == 'JEQ':
+                # If equal flag is set (true), jump to the address
+                # stored in the given register.
+                if self.flag == 0b00000001:
+                    self.pc = self.register[oper_a]
+                else:
+                    self.pc += 2
+
+            elif IR == 'JNE':
+                # If E flag is clear (false, 0), jump to the address
+                # stored in the given register.
+                if self.flag != 0b00000001:
+                    self.pc = self.register[oper_a]
+                else:
+                    self.pc += 2
+
+            else:
+                print('function not supported')
+                self.pc += 1
+
+cpu = CPU()
+
+cpu.load(sys.argv[1])
+cpu.run()
